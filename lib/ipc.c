ea7e17a1bfd61f9b9b109b936805d11c9d9f8ebd
Argha Badhon Saha <saha56@eceprog4.ecn.purdue.edu>
2024-04-02 02:25:43 -0400

    ipc_send function

--- lib/ipc.c
+++ lib/ipc.c
@@ -67,7 +67,21 @@ void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
 	// LAB 4: Your code here.
-	panic("ipc_send not implemented");
+	// panic("ipc_send not implemented");
+
+	int r;
+	
+	if (!pg) {
+		pg = (void *) UTOP;
+	}
+
+	while ((r = sys_ipc_try_send(to_env, val, pg, perm)) < 0) {
+		if (r != -E_IPC_NOT_RECV) {
+			panic("ipc_send failed: %e\n", r);
+		}
+
+		sys_yield();
+	}
 }
 
 // Find the first environment of the given type.  We'll use this to
