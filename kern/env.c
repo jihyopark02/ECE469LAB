3cec6ac518881e67d6dbdf45721ef0e2b89a50bf
Argha Badhon Saha <saha56@eceprog5.ecn.purdue.edu>
2024-04-04 17:25:49 -0400

    Completed Lab 5 Questions

--- kern/env.c
+++ kern/env.c
@@ -570,7 +570,7 @@ env_run(struct Env *e)
 	curenv -> env_status = ENV_RUNNING;
 	curenv -> env_runs++;
 
-	lcr3(PADDR(curenv -> env_pgdir));
+	lcr3(PADDR(e -> env_pgdir));
 
 	unlock_kernel();
 	env_pop_tf(&curenv -> env_tf);
